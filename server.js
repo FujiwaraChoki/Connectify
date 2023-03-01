ad608a1bf2a40ecf1ae456713c02b70af0628161
Sami Hindi <lolipop66611@gmail.com>
2023-03-01 14:28:46 +0100

    Added all hosting names (0.0.0.0)

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
 });
